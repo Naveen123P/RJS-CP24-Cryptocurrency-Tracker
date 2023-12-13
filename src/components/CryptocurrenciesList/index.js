cbae67211d0c11dabeaca91c265054a86461f1ce
Naveen123P <125892820+Naveen123P@users.noreply.github.com>
2023-12-13 19:51:19 +0530

    Update index.js

--- src/components/CryptocurrenciesList/index.js
+++ src/components/CryptocurrenciesList/index.js
@@ -1,10 +1,12 @@
 // Write your JS code here
 import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 import CryptocurrencyItem from '../CryptocurrencyItem'
 import './index.css'
 
 class CryptocurrenciesList extends Component {
-  state = {cryptocurrenciesList: []}
+  state = {cryptocurrenciesList: [], isLoading: true}
 
   componentDidMount() {
     this.getCryptocurrenciesList()
@@ -22,33 +24,43 @@ class CryptocurrenciesList extends Component {
       id: each.id,
       currencyLogo: each.currency_logo,
     }))
-    this.setState({cryptocurrenciesList: formattedData})
+    this.setState({cryptocurrenciesList: formattedData, isLoading: false})
   }
 
   render() {
-    const {cryptocurrenciesList} = this.state
+    const {cryptocurrenciesList, isLoading} = this.state
 
     return (
-      <div className="bg-container2">
-        <h1 className="crypto-heading">Cryptocurrency Tracker</h1>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png"
-          alt="cryptocurrency"
-          className="crypto-img"
-        />
-        <ul className="cryptocurrency-container" data-testid="loader">
-          <li className="table-header">
-            <p className="coin-type-title">Coin Type</p>
-            <div className="usd-euro-container">
-              <p className="usd-title">USD</p>
-              <p className="euro-title">EURO</p>
-            </div>
-          </li>
-          {cryptocurrenciesList.map(eachItem => (
-            <CryptocurrencyItem key={eachItem.id} cryptoDetails={eachItem} />
-          ))}
-        </ul>
-      </div>
+      <>
+        {isLoading ? (
+          <Loader type="Circles" color="#ffffff" height={20} width={20} />
+        ) : (
+          <div className="bg-container2" data-testid="loader">
+            <h1 className="crypto-heading">Cryptocurrency Tracker</h1>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png"
+              alt="cryptocurrency"
+              className="crypto-img"
+            />
+            <ul className="cryptocurrency-container">
+              <li className="table-header">
+                <p className="coin-type-title">Coin Type</p>
+                <div className="usd-euro-container">
+                  <p className="usd-title">USD</p>
+                  <p className="euro-title">EURO</p>
+                </div>
+              </li>
+
+              {cryptocurrenciesList.map(eachItem => (
+                <CryptocurrencyItem
+                  key={eachItem.id}
+                  cryptoDetails={eachItem}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+      </>
     )
   }
 }
